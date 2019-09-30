a89e3ad0d5522d87e193fcf359d6012f2e2c7247
Darren SooHoo <darsoohoo@gmail.com>
2019-09-29 23:12:52 -0700

    updatE

--- app/arrays.js
+++ app/arrays.js
@@ -24,7 +24,12 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy: function(arr, item) {
-   
+   for (let i = 0; i < arr.length; i++) {
+     if (arr[i] === item) {
+       arr.splice(i, 1)
+     }
+   }
+   return arr;
   },
 
   append: function(arr, item) {
